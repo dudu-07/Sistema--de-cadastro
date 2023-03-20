4ba3e94f450b284cecc8e1ebc0ff12a22ad91ecd
dudu-07 <edu_matheus072@hotmail.com>
2023-03-20 13:52:04 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamneto de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Enviando para o github")
+console.log('modificação feita pelo dudu')
